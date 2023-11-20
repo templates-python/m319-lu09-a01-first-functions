98ead95377c6d7ed5211323a4814e10c21049c87
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-11-20 14:36:22 +0100

    done

--- calculator.py
+++ calculator.py
@@ -1,10 +1,66 @@
-def add(TODO):
-    ...
+def add(x, y):
+    """
+    Add two numbers.
+    :param x: Number
+    :param y: Number
+    :return: Sum of x and y
+    """
+    return x + y
 
+def subtract(x, y):
+    """
+    Subtract y from x.
+    :param x: Number
+    :param y: Number
+    :return: Difference of x and y
+    """
+    return x - y
+
+def multiply(x, y):
+    """
+    Multiply x by y.
+    :param x: Number
+    :param y: Number
+    :return: Product of x and y
+    """
+    return x * y
+
+def divide(x, y):
+    """
+    Divide x by y.
+    :param x: Number
+    :param y: Number
+    :return: Quotient of x and y
+    """
+    return x / y if y != 0 else 'Division by zero'
+
+def power(x, y):
+    """
+    Calculate x raised to the power of y.
+    :param x: Base number
+    :param y: Exponent
+    :return: x raised to the power of y
+    """
+    return x ** y
+
+def root(x, y):
+    """
+    Calculate the y-th root of x.
+    :param x: Number
+    :param y: Root
+    :return: y-th root of x
+    """
+    return x ** (1 / y) if y != 0 else 'Root by zero'
 
 def main():
-    total = add(5, 5.5)
-    print(total)
+    print(add(5, 5.5))
+    print(subtract(5, 3))
+    print(multiply(5, 3))
+    print(divide(6, 3))
+    print(divide(5, 0))
+    print(power(2, 3))
+    print(root(27, 3))
+    print(root(16, 0))
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -6,6 +6,32 @@ def function1():
     print('Function 1 is called')
 
 
+def function2():
+    """
+    Function without params, but returns a value
+    :return: str
+    """
+    return 'Function 2 is called'
+
+
+def function3(argument):
+    """
+    Function with a param and no return
+    :param argument: str
+    :return: None
+    """
+    print(f'Function 3 is called with argument: {argument}')
+
+
+def function4(argument):
+    """
+    Function with a param and a return
+    :param argument: str
+    :return: str
+    """
+    return f'Function 4 is called with argument: {argument}'
+
+
 def four_functions():
     """
     Main function
@@ -13,8 +39,11 @@ def four_functions():
     """
     function1()
     received_from_2 = function2()
+    print(received_from_2)
     function3('passed Argument to print in function3')
     received_from_4 = function4('passed Argument to print in function4')
+    print(received_from_4)
+
 
 
 if __name__ == '__main__':
